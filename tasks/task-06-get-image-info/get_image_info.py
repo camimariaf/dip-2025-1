6114fef9065237e039476a025e8529708ba3c70f
camimariaf <123339557+camimariaf@users.noreply.github.com>
2025-08-24 22:54:03 -0300

    Update get_image_info.py

--- tasks/task-06-get-image-info/get_image_info.py
+++ tasks/task-06-get-image-info/get_image_info.py
@@ -40,11 +40,12 @@ def get_image_info(image):
     # Handle grayscale vs multi-channel separately
     if depth == 1:
         # Single-channel
-        info["statistics"]["min"] = None
-        info["statistics"]["max"] = None
-        info["statistics"]["mean"] = None
-        info["statistics"]["std_dev"] = None
-        info["statistics"]["median"] = None
+        info["statistics"]["min"] = int(np.min(image))
+        info["statistics"]["max"] = int(np.max(image))
+        info["statistics"]["mean"] = float(np.mean(image))
+        info["statistics"]["std_dev"] = float(np.std(image))
+        info["statistics"]["median"] = float(np.median(image))
+        
         hist, bins = np.histogram(image, bins=256, range=(0, 256))
         info["statistics"]["histogram"] = hist.tolist()
     else:
@@ -52,11 +53,11 @@ def get_image_info(image):
         channels = cv2.split(image)
         for idx, ch in enumerate(channels):
             ch_stats = {
-                "min": None,
-                "max": None,
-                "mean": None,
-                "std_dev": None,
-                "median": None,
+                "min": int(np.min(ch)),
+                "max": int(np.max(ch)),
+                "mean": float(np.mean(ch)),
+                "std_dev": float(np.std(ch)),
+                "median": float(np.median(ch))
             }
             hist, bins = np.histogram(ch, bins=256, range=(0, 256))
             ch_stats["histogram"] = hist.tolist()
